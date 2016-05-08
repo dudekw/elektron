281c85f7d20aae0b6287920a22a0dc125293fb5c
dudekw <wojciechsbox@gmail.com>
2016-05-09 00:16:43 +0200

    add basic method calls

--- elektron_apps/Rapps/src/navigation_test.cpp
+++ elektron_apps/Rapps/src/navigation_test.cpp
@@ -1,9 +1,29 @@
 #include "rapp-robots-api/navigation/navigation.hpp"
-
+#include "rapp/objects/poseStamped/poseStamped.hpp"
+#include <iostream>
 int main(int argc, char ** argv){
 
 	rapp::robot::navigation nav;
 	nav.moveStop();
+	std::vector<std::string> joint_names;
+	joint_names.clear();
+	joint_names.push_back("head_pitch");
+
+	std::vector<float> joint_angles;
+	joint_angles.clear();
+	joint_angles.push_back(0);
+
+	nav.moveJoint(joint_names, joint_angles);
+
+	rapp::object::PoseStamped pose;
+	std::vector<rapp::object::PoseStamped> poses_list;
+	poses_list.clear();
+	poses_list.push_back(pose);
+	poses_list.push_back(pose);
+	std::cout<<poses_list.at(0).pose.position.x<<std::endl;
+
+	std::cout<<poses_list.size()<<std::endl;
+	nav.moveAlongPath(poses_list);
 
 	return 0;
 }
