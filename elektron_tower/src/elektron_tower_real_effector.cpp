511e5770df69220be4eed57ba4a2b6c0b2ca4eb4
robot <robot@elektron1.robotyka.ia.pw.edu.pl>
2016-05-18 12:18:32 +0200

    fix tower_effector driver

--- elektron_tower/src/elektron_tower_real_effector.cpp
+++ elektron_tower/src/elektron_tower_real_effector.cpp
@@ -6,12 +6,14 @@
 #include <ros/ros.h>
 #include <stdio.h> 
 #include <unistd.h>
-#include <fcntl.h>
 #include <sstream>
 #include <string.h>
 #include <elektron_msgs/MoveTower.h>
 #include <ros/console.h>
 #include <tf/transform_broadcaster.h>
+#include <fcntl.h>
+#include <termios.h>
+
 
 int fd;
 int orientation_yaw = 0;
@@ -20,6 +22,29 @@ std::string toString(double d) {
 	std::stringstream ss;
 	ss << d;
 	return ss.str();
+}
+bool setConnection(std::string elektron_tower_device){
+
+//char port[20] = elektron_tower_device.c_str(); /* port to connect to */
+speed_t baud = B9600; /* baud rate */
+
+fd = open(elektron_tower_device.c_str(), O_WRONLY); /* connect to port */
+
+/* set the other settings (in this case, 9600 8N1) */
+struct termios settings;
+tcgetattr(fd, &settings);
+
+cfsetospeed(&settings, baud); /* baud rate */
+settings.c_cflag &= ~PARENB; /* no parity */
+settings.c_cflag &= ~CSTOPB; /* 1 stop bit */
+settings.c_cflag &= ~CSIZE;
+settings.c_cflag |= CS8 | CLOCAL; /* 8 bits */
+settings.c_lflag = ICANON; /* canonical mode */
+settings.c_oflag &= ~OPOST; /* raw output */
+
+tcsetattr(fd, TCSANOW, &settings); /* apply the settings */
+//tcflush(fd, TCOFLUSH);
+std::cout<<fd<<std::endl;
 }
 bool sendAngles( int yaw, int pitch){
 		const char* message_char;
@@ -52,16 +77,16 @@ bool moveTower(elektron_msgs::MoveTower::Request  &req,
          elektron_msgs::MoveTower::Response &res)
 {
 	if (req.moveJoints.size()==2){
-		res.status = sendAngles((int) (req.yaw*180/3.14), (int) (req.pitch*180/3.14));
+		res.status = sendAngles((int) (req.yaw*180/3.14+90), (int) ((req.pitch+0.73)*180/3.14));
 		orientation_yaw = req.yaw;
 		orientation_pitch = req.pitch;
 	}
 	if (req.moveJoints.at(0)=="yaw"){
-		res.status = sendAngles((int) (req.yaw*180/3.14), (int) (req.pitch*180/3.14));
+		res.status = sendAngles((int) (req.yaw*180/3.14+90), (int) ((req.pitch+0.73)*180/3.14));
 		orientation_yaw = req.yaw;
 	}
 	if (req.moveJoints.at(0)=="pitch"){
-		res.status = sendAngles((int) (req.yaw*180/3.14), (int) (req.pitch*180/3.14));
+		res.status = sendAngles((int) (req.yaw*180/3.14+90), (int) ((req.pitch+0.73)*180/3.14));
 		orientation_pitch = req.pitch;
 	}
 	return false;
@@ -73,12 +98,11 @@ int main(int argc, char** argv){
 	tf::TransformBroadcaster br;
 	tf::Transform yaw_transform;
 	tf::Transform pitch_transform;
-
-	ros::Rate rate(10.0);
 	std::string elektron_tower_device;
-	nh.param<std::string>("elektron_tower_device", elektron_tower_device, "/dev/ttyACM0");
+	nh.param<std::string>("elektron_tower_device", elektron_tower_device, "/dev/ttyUSB0");
+	setConnection(elektron_tower_device);
+	ros::Rate rate(10.0);
 	std::cout<<elektron_tower_device<<std::endl;
-	fd = open(elektron_tower_device.c_str(),O_WRONLY);
 	ros::ServiceServer service = nh.advertiseService("re_moveTowerJoint", moveTower);
 	if(fd!=-1){
 		ROS_INFO("Ready to move the tower.");
