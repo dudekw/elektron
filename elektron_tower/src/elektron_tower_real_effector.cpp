f6f255b40c9a2abb77ecc391b69f350cc404890b
DudekW <wojciechsbox@gmail.com>
2016-05-19 21:14:43 +0200

    real elektron tf fix, new tower mesh

--- elektron_tower/src/elektron_tower_real_effector.cpp
+++ elektron_tower/src/elektron_tower_real_effector.cpp
@@ -13,11 +13,11 @@
 #include <tf/transform_broadcaster.h>
 #include <fcntl.h>
 #include <termios.h>
-
+#include <sensor_msgs/JointState.h>
 
 int fd;
-int orientation_yaw = 0;
-int orientation_pitch = 0;
+double orientation_yaw = 0;
+double orientation_pitch = 0;
 std::string toString(double d) {
 	std::stringstream ss;
 	ss << d;
@@ -63,14 +63,63 @@ bool sendAngles( int yaw, int pitch){
 	}
 	return false;
 }
-bool publishFrames(tf::Transform yaw_transform, tf::Transform pitch_transform,tf::TransformBroadcaster br){
+bool publishJoints(sensor_msgs::JointState joint_states, ros::Publisher joint_pub, std::vector<double> joints_positions,std::vector<std::string> joints_names, std::vector<double> joints_efforts, std::vector<double> joints_velocities){
+	
 
-    yaw_transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-    yaw_transform.setRotation( tf::Quaternion(0, 0, orientation_yaw, 1) );
-    br.sendTransform(tf::StampedTransform(yaw_transform, ros::Time::now(), "head_bottom_fixed", "head_yaw_revolute"));
-    pitch_transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-    pitch_transform.setRotation( tf::Quaternion(0, 0, orientation_pitch, 1) );
-    br.sendTransform(tf::StampedTransform(pitch_transform, ros::Time::now(), "head_upper_fixed", "head_pitch_revolute"));
+	
+	joints_positions.push_back(0);
+	joints_positions.push_back(0);
+	joints_positions.push_back(0);
+	joints_positions.push_back(0);
+	joints_positions.push_back(0);
+	joints_positions.push_back(0);
+	joints_positions.push_back(orientation_yaw);
+	joints_positions.push_back(orientation_pitch);
+
+joints_names.push_back("base_link_left_wheel_joint");
+joints_names.push_back("base_link_left_front_wheel_joint");
+joints_names.push_back("base_link_left_rear_wheel_joint");
+joints_names.push_back("base_link_right_wheel_joint");
+joints_names.push_back("base_link_right_front_wheel_joint");
+joints_names.push_back("base_link_right_rear_wheel_joint");
+joints_names.push_back("head_yaw_revolute_joint_1");
+joints_names.push_back("head_pitch_revolute_joint_1");
+
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+	joints_efforts.push_back(0);
+
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joints_velocities.push_back(0);
+	joint_states.header.stamp = ros::Time::now();
+	joint_states.name = joints_names;
+	joint_states.position = joints_positions;
+	joint_states.effort = joints_efforts;
+	joint_states.velocity = joints_velocities;
+
+	joint_pub.publish(joint_states);
+	joints_names.clear();
+	joints_positions.clear();
+	joints_efforts.clear();
+	joints_velocities.clear();
+
+//   yaw_transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+ //   yaw_transform.setRotation( tf::Quaternion(0, 0, orientation_yaw, 1) );
+ //   br.sendTransform(tf::StampedTransform(yaw_transform, ros::Time::now(), "head_bottom_fixed_link_1", "head_yaw_revolute_joint_1"));
+ //   pitch_transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+ //   pitch_transform.setRotation( tf::Quaternion(0, 0, orientation_pitch, 1) );
+ //   br.sendTransform(tf::StampedTransform(pitch_transform, ros::Time::now(), "head_upper_fixed_link_1", "head_pitch_revolute_joint_1"));
 }
 
 bool moveTower(elektron_msgs::MoveTower::Request  &req,
@@ -96,6 +145,14 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "elektron_tower_effector");
 	ros::NodeHandle nh;
 	tf::TransformBroadcaster br;
+sensor_msgs::JointState joint_states;
+	ros::Publisher joint_pub = nh.advertise<sensor_msgs::JointState>("elektron/joint_states", 10);
+	std::vector<std::string> joints_names;
+	joints_names.clear();
+	std::vector<double> joints_positions;
+	std::vector<double> joints_efforts;
+	std::vector<double> joints_velocities;
+	joints_positions.clear();
 	tf::Transform yaw_transform;
 	tf::Transform pitch_transform;
 	std::string elektron_tower_device;
@@ -104,16 +161,16 @@ int main(int argc, char** argv){
 	ros::Rate rate(10.0);
 	std::cout<<elektron_tower_device<<std::endl;
 	ros::ServiceServer service = nh.advertiseService("re_moveTowerJoint", moveTower);
-	if(fd!=-1){
+	//if(fd!=-1){
 		ROS_INFO("Ready to move the tower.");
 		while (nh.ok()){
-		    publishFrames(yaw_transform,pitch_transform,br);
+		    publishJoints(joint_states,joint_pub,joints_positions,joints_names, joints_efforts,joints_velocities);
 		    rate.sleep();
 		    ros::spinOnce();
 		}
-	}else{
+	//}else{
 		ROS_ERROR("Elektron Tower is disconected from the robot!");
 		return -1;
-	}
+	//}
 	return 0;
 }
