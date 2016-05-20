2341d844f36a0d5d05a8adfe1782af40fc8ec93a
DudekW <wojciechsbox@gmail.com>
2016-05-20 11:05:07 +0200

    add capture image support

--- elektron_base/virtual_receptors/scripts/cameras_controller.py
+++ elektron_base/virtual_receptors/scripts/cameras_controller.py
@@ -38,7 +38,7 @@ class CamerasModule():
 	"""
 	
 	# Constructor of MoveNaoModule
-	def __init__(self,name,topic_name):
+	def __init__(self,name,enable_head,enable_top_kinect,enable_bottom_kinect):
 		print "[Cameras VR] - Virtual receptor <<Cameras>> initialization"
 		
 		# Initialization of ROS node
@@ -46,7 +46,7 @@ class CamerasModule():
 		
 		# Initialization of Naoqi modules and ROS services
 		self.checkSubsystems()
-		self.setVariables(topic_name)
+		self.setVariables(enable_head,enable_top_kinect,enable_bottom_kinect)
 
 		self.openServices()
 
@@ -58,12 +58,22 @@ class CamerasModule():
 		print "[Cameras VR] - Checking existance of real receptors -> not implemented"
 
 	# Setting variables
-	def setVariables(self,topic_name):
+	def setVariables(self,enable_head,enable_top_kinect,enable_bottom_kinect):
 		try:
-			rospy.wait_for_message(topic_name,Image,timeout=4)
 			print "[Cameras VR] - Setting variables"
-			self.topic_name = topic_name
-			image_sub = rospy.Subscriber(topic_name, Image, self.writeImage)
+
+			if (enable_head=="1"):
+				topic_name_head = "/head_camera_rgb/image_raw"
+				rospy.wait_for_message(topic_name_head,Image,timeout=4)
+				image_sub_head = rospy.Subscriber(topic_name_head, Image, self.writeImage_head)
+			if (enable_top_kinect=="1"):
+				topic_name_top_kinect = "/tower_top_kinect/rgb/image_raw"
+				rospy.wait_for_message(topic_name_top_kinect,Image,timeout=4)
+				image_sub_top_kinect = rospy.Subscriber(topic_name_top_kinect, Image, self.writeImage_top_kinect)
+			if (enable_bottom_kinect=="1"):
+				topic_name_bottom_kinect = "/bottom_kinect/rgb/image_raw"
+				rospy.wait_for_message(topic_name_bottom_kinect,Image,timeout=4)
+				image_sub_bottom_kinect = rospy.Subscriber(topic_name_bottom_kinect, Image, self.writeImage_bottom_kinect)
 		except(rospy.ROSException), e:
 			print "Camete image topic not available, aborting..."
 			print "Error message: ", e
@@ -75,8 +85,12 @@ class CamerasModule():
 			self.service_map = rospy.Service('rapp_capture_image', GetImage, self.rapp_captrure_image_interface)
 		except Exception, ex_map:
 			print "[Cameras VR] - Exception %s" % str(ex_map)
-	def writeImage(self,msg):
-		self.image = msg
+	def writeImage_head(self,msg):
+		self.image_head = msg
+	def writeImage_top_kinect(self,msg):
+		self.image_top_kinect = msg
+	def writeImage_bottom_kinect(self,msg):
+		self.image_bottom_kinect = msg
 	def getch(self):
 		import sys, tty, termios
 		fd = sys.stdin.fileno()
@@ -93,8 +107,13 @@ class CamerasModule():
 ####
 
 	def rapp_captrure_image_interface(self,req):
-		
-		return GetImageResponse(self.image)
+		if (req.camera_id==0):		
+			pub_img = self.image_head
+		if (req.camera_id==1):		
+			pub_img = self.image_bottom_kinect
+		if (req.camera_id==2):		
+			pub_img = self.image_top_kinect
+		return GetImageResponse(pub_img)
 
 # Testng SIGINT signal handler
 def signal_handler(signal, frame):
@@ -102,7 +121,7 @@ def signal_handler(signal, frame):
 	print "[Cameras VR] - system exits"
 	sys.exit(0)
 
-def main(node_name,topic_name):
+def main(node_name,enable_head,enable_top_kinect,enable_bottom_kinect):
 	""" Main entry point
 		"""
 	try:
@@ -111,7 +130,7 @@ def main(node_name,topic_name):
 		#self.use_sim = use_sim
 		rospy.init_node("acore_cameras_receptor_"+node_name)
 		global ElektronCameras
-		ElektronCameras = CamerasModule("ElektronCameras",topic_name)
+		ElektronCameras = CamerasModule("ElektronCameras",enable_head,enable_top_kinect,enable_bottom_kinect)
 		rospy.spin()
 	
 	except (KeyboardInterrupt, SystemExit):
@@ -127,9 +146,9 @@ def main(node_name,topic_name):
 		
 if __name__ == "__main__":
 	try:
-		if len(sys.argv) < 3:
-			print("usage: cameras_controller <<node_name>> <<camera_topic_name>>")
+		if len(sys.argv) < 5:
+			print("usage: cameras_controller <<node_name>> <<enable_head>> <<enable_top_kinect>> <<enable_bottom_kinect>>")
 		else:
-			main(sys.argv[1],sys.argv[2])
+			main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 	except Exception,e:
 		print "__name__ - Error %s" % str(e)
