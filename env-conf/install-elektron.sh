3152bb6e25c10756a5ce370c9b9091b81ac0c8da
DudekW <wojciechsbox@gmail.com>
2016-05-20 17:05:19 +0200

    add rapp-cloud-api to installation script

--- env-conf/install-elektron.sh
+++ env-conf/install-elektron.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
 
 sudo apt-get install -y ros-indigo-desktop-full;
 sudo apt-get remove -y gazebo2;
