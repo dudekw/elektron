052fe414b69f7b9edafe397aabc1e2cdbc7fbb87
dudekw <wojciechsbox@gmail.com>
2016-06-05 15:44:41 +0200

    fixes simulation installation

--- env-conf/install-elektron-physical.sh
+++ env-conf/install-elektron-physical.sh
@@ -37,7 +37,7 @@ else
 fi
 
 cd ~/rapp/robots/src/elektron;
-rm elektron_base/elektron-real-effectors/CMakeLists.txt;
+
 git submodule update --init --recursive elektron_base/elektron-real-effectors netusb_camera_driver;
 source /opt/ros/indigo/setup.bash;
 cd ../..;
