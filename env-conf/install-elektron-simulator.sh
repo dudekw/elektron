0efa935f7f75cbc2e1edc9e47cf893fa390c9354
wojciech <wojciechsbox@gmail.com>
2017-07-01 22:32:43 +0200

    use gazebo7

--- env-conf/install-elektron-simulator.sh
+++ env-conf/install-elektron-simulator.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+wget -O install_packages.sh https://raw.githubusercontent.com/dudekw/elektron/kinetic/env-conf/install_packages.sh;
 bash install_packages.sh
 
-sudo apt-get install -y libgazebo5-dev;
-sudo apt-get install -y gazebo5;
+sudo apt-get install -y libgazebo7-dev;
+sudo apt-get install -y gazebo7;
 
 mkdir -p ~/rapp/robots/src;
 cd ~/rapp/robots/src;
@@ -17,7 +18,7 @@ fi
 cd ~/rapp/robots/src/elektron;
 touch elektron_base/elektron-real-effectors/CMakeLists.txt;
 git submodule update --init --recursive elektron-simulation;
-source /opt/ros/indigo/setup.bash;
+source /opt/ros/kinetic/setup.bash;
 cd ../..;
 
 if [ -d "install" ]; then
