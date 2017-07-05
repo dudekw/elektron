690375d16b690e62dab382fd529c41410a0d3b4b
dudekw <wojciechsbox@gmail.com>
2017-07-05 13:41:00 +0200

    remove -y param in pkgs install, remove kinetic-desktop pkg

--- env-conf/install_packages.sh
+++ env-conf/install_packages.sh
@@ -2,32 +2,32 @@
 
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install -y git;
-sudo apt-get install -y libzbar-dev;
-sudo apt-get install -y ros-kinetic-desktop;
-sudo apt-get install -y ros-kinetic-std-srvs;
-sudo apt-get install -y python-catkin-tools;
-sudo apt-get install -y ros-kinetic-control-toolbox;
-sudo apt-get install -y ros-kinetic-controller-manager;
-sudo apt-get install -y ros-kinetic-transmission-interface;
-sudo apt-get install -y ros-kinetic-joint-limits-interface ;
-sudo apt-get install -y ros-kinetic-joint-state-controller ;
-sudo apt-get install -y ros-kinetic-diff-drive-controller ;
-sudo apt-get install -y ros-kinetic-effort-controllers ;
-sudo apt-get install -y ros-kinetic-position-controllers;
-sudo apt-get install -y ros-kinetic-image-transport;
-sudo apt-get install -y ros-kinetic-cv-bridge;
-sudo apt-get install -y ros-kinetic-eigen-conversions;
-sudo apt-get install -y ros-kinetic-tf-conversions;
-sudo apt-get install -y ros-kinetic-nodelet;
-sudo apt-get install -y ros-kinetic-driver-base;
-sudo apt-get install -y ros-kinetic-polled-camera;
-sudo apt-get install -y ros-kinetic-camera-info-manager;
-sudo apt-get install -y ros-kinetic-ros-core;
-sudo apt-get install -y ros-kinetic-xacro;
-sudo apt-get install -y ros-kinetic-robot-state-publisher;
-sudo apt-get install -y ros-kinetic-scan-tools;
-sudo apt-get install -y ros-kinetic-yocs-cmd-vel-mux;
-sudo apt-get install -y ros-kinetic-navigation;
+sudo apt-get install git;
+sudo apt-get install libzbar-dev;
+#sudo apt-get install -y ros-kinetic-desktop;
+sudo apt-get install ros-kinetic-std-srvs;
+sudo apt-get install python-catkin-tools;
+sudo apt-get install ros-kinetic-control-toolbox;
+sudo apt-get install ros-kinetic-controller-manager;
+sudo apt-get install ros-kinetic-transmission-interface;
+sudo apt-get install ros-kinetic-joint-limits-interface ;
+sudo apt-get install ros-kinetic-joint-state-controller ;
+sudo apt-get install ros-kinetic-diff-drive-controller ;
+sudo apt-get install ros-kinetic-effort-controllers ;
+sudo apt-get install ros-kinetic-position-controllers;
+sudo apt-get install ros-kinetic-image-transport;
+sudo apt-get install ros-kinetic-cv-bridge;
+sudo apt-get install ros-kinetic-eigen-conversions;
+sudo apt-get install ros-kinetic-tf-conversions;
+sudo apt-get install ros-kinetic-nodelet;
+sudo apt-get install ros-kinetic-driver-base;
+sudo apt-get install ros-kinetic-polled-camera;
+sudo apt-get install ros-kinetic-camera-info-manager;
+sudo apt-get install ros-kinetic-ros-core;
+sudo apt-get install ros-kinetic-xacro;
+sudo apt-get install ros-kinetic-robot-state-publisher;
+sudo apt-get install ros-kinetic-scan-tools;
+sudo apt-get install ros-kinetic-yocs-cmd-vel-mux;
+sudo apt-get install ros-kinetic-navigation;
 
 
